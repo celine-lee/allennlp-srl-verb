a965b1be3804b281ac78dac5315c8793ef1a588a
Celine Y Lee <celinel@orwell.seas.upenn.edu>
2020-05-18 13:10:36 -0400

    add relevant smaller files

---
+++ set_paths.sh
@@ -0,0 +1,3 @@
+export SRL_TRAIN_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0"
+export SRL_VALIDATION_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0"
+export CUDA_VISIBLE_DEVICES=0,1
