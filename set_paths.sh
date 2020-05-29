fe6d2779201d5bfde140f6f592bbfab9aca1c37d
celine-lee <celine.y.lee@gmail.com>
2020-05-29 10:03:41 -0400

    Fix training and development paths

--- set_paths.sh
+++ set_paths.sh
@@ -1,3 +1,3 @@
-export SRL_TRAIN_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0"
-export SRL_VALIDATION_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0"
+export SRL_TRAIN_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data/train"
+export SRL_VALIDATION_DATA_PATH="/shared/celinel/LDC2013T19/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data/development"
 export CUDA_VISIBLE_DEVICES=0,1
